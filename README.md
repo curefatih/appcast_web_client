257b69bdc5154ffb90cf1a03e5e2c1a0327b0fe1
Fatih Cüre <curefat@gmail.com>
2019-08-16 20:44:22 +0300

    initial

--- README.md
+++ README.md
@@ -27,3 +27,8 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
+### copyrights
+
+  -welcome photo Photo by Brett Jordan from Pexels
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Welcome/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Welcome from './pages/Welcome.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Welcome
   }
 }
 </script>
 
 <style>
+html, body{
+  width: 100%;
+  height: 100%;
+}
+body{
+  background-color: #292929;
+}
+*{
+  box-sizing: border-box;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #fff;
+  height: 100%;
+}
+:root{
+  --primary: #263238;
+  --primary-dark: #000a12;
+  --primary-light: #4f5b62;
 }
 </style>
