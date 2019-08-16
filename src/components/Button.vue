cc26a5d1c572b68c9a3ef3941063fb4e2519c1a6
Fatih Cüre <curefat@gmail.com>
2019-08-16 20:45:26 +0300

    initial button

---
+++ src/components/Button.vue
@@ -0,0 +1,28 @@
+<template>
+  <button class="button" :type="type">{{value}}</button>
+</template>
+
+<script>
+export default {
+  props:{
+    type: String,
+    value: String
+  }
+}
+</script>
+
+<style>
+.button{
+  background-color: var(--primary-dark);
+  border: 1px solid #ddd;
+  color: #fff;
+  padding: 10px 25px;
+  border-radius: 2em;
+  opacity: .5;
+  cursor: pointer;
+  transition: .2s all;
+}
+.button:hover{
+  opacity: 1;
+}
+</style>
