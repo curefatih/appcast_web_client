bc9b3236e00cca91ff86c4358b1d245ee827dd46
Fatih Cüre <curefat@gmail.com>
2019-09-02 19:47:10 +0300

    Dashboard page initial

---
+++ src/pages/Dashboard.vue
@@ -0,0 +1,148 @@
+<template>
+  <div class="dashboard">
+    <div class="content">
+      <div class="header">
+        <h1>Last activities</h1>
+      </div>
+
+      <div class="post-content">
+        <div class="wrap xl-gutter-16">
+          <div class="post col xl-4-12">
+            <div class="post-user-info">
+              <div class="wrap xl-flexbox xl-gutter-8 xl-middle">
+                <div class="user-pp">
+                  <img src="../assets/logo.png" alt />
+                </div>
+                <div class="col user-identical">
+                  <h6>Fatih Cüre</h6>
+                  <a>@curefatih</a>
+                </div>
+              </div>
+            </div>
+            <div class="col xl-1-1 post-box">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate labore reprehenderit eaque eveniet eum corrupti quae dignissimos delectus? Corporis consequuntur voluptatem, placeat temporibus ducimus vitae doloremque perspiciatis veniam qui excepturi.</p>
+            </div>
+          </div>
+        </div>
+        <div class="wrap xl-gutter-16">
+          <div class="post col xl-4-12">
+            <div class="post-user-info">
+              <div class="wrap xl-flexbox xl-gutter-8 xl-middle">
+                <div class="user-pp">
+                  <img src="../assets/logo.png" alt />
+                </div>
+                <div class="col user-identical">
+                  <h6>Fatih Cüre</h6>
+                  <a>@curefatih</a>
+                </div>
+              </div>
+            </div>
+            <div class="col xl-1-1 post-box">
+              <progressive-img src="/static/welcome.jpg" />
+              <!-- <img src="../assets/somepicture.jpg" alt /> -->
+            </div>
+          </div>
+        </div>
+        <div class="wrap xl-gutter-16">
+          <div class="post col xl-4-12">
+            <div class="post-user-info">
+              <div class="wrap xl-flexbox xl-gutter-8 xl-middle">
+                <div class="user-pp">
+                  <img src="../assets/logo.png" alt />
+                </div>
+                <div class="col user-identical">
+                  <h6>Fatih Cüre</h6>
+                  <a>@curefatih</a>
+                </div>
+              </div>
+            </div>
+            <div class="col xl-1-1 post-box">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate labore reprehenderit eaque eveniet eum corrupti quae dignissimos delectus? Corporis consequuntur voluptatem, placeat temporibus ducimus vitae doloremque perspiciatis veniam qui excepturi.</p>
+            </div>
+          </div>
+          <div class="post col xl-4-12">
+            <div class="post-user-info">
+              <div class="wrap xl-flexbox xl-gutter-8 xl-middle">
+                <div class="user-pp">
+                  <img src="../assets/logo.png" alt />
+                </div>
+                <div class="col user-identical">
+                  <h6>Fatih Cüre</h6>
+                  <a>@curefatih</a>
+                </div>
+              </div>
+            </div>
+            <div class="col xl-1-1 post-box">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate labore reprehenderit eaque eveniet eum corrupti quae dignissimos delectus? Corporis consequuntur voluptatem, placeat temporibus ducimus vitae doloremque perspiciatis veniam qui excepturi.</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate labore reprehenderit eaque eveniet eum corrupti quae dignissimos delectus? Corporis consequuntur voluptatem, placeat temporibus ducimus vitae doloremque perspiciatis veniam qui excepturi.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Dashboard"
+};
+</script>
+
+<style>
+.content {
+  padding: 50px 0;
+}
+
+.header {
+  border-bottom: 1px solid var(--secondary-dark);
+  margin-bottom: 30px;
+}
+.header h1 {
+  position: relative;
+  display: inline-block;
+}
+.header h1::before {
+  content: "";
+  width: 100%;
+  position: absolute;
+  bottom: -0.6em;
+  z-index: 2;
+  box-sizing: content-box;
+  border-bottom: 1px solid var(--blue);
+}
+
+.post {
+  margin-bottom: 20px;
+}
+.user-pp img {
+  max-width: 30px;
+  background-color: #0008;
+  border: 1px solid #0005;
+  padding: 3px;
+  border-radius: 0.5rem;
+}
+
+.user-identical h6 {
+  margin: 0;
+}
+.user-identical a {
+  font-size: 0.8em;
+}
+
+.post-box {
+  font-size: 12px !important;
+  font-weight: 100;
+  background-color: var(--secondary-dark);
+  margin-top: 10px;
+  padding: 15px 10px;
+  border-radius: 0.3em;
+}
+.post-box img {
+  width: 100%;
+  height: 100%;
+  max-height: 400px;
+  object-fit: contain;
+  /* object-fit: scale-down; */
+}
+</style>
