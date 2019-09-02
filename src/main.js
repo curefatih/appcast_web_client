b74f1708df064ccf29318930f18d4aecdde61c41
Fatih Cüre <curefat@gmail.com>
2019-09-02 12:46:51 +0300

    Routes moved to ./router/index.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router';
+import VueProgressiveImage from 'vue-progressive-image'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(VueProgressiveImage, {
+  cache: false
+});
 
 import './assets/reset.css';
 import './assets/flex.css';
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
