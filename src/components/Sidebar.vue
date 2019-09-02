4e81eba06dc940beb32243ad37142acaa23a868f
Fatih Cüre <curefat@gmail.com>
2019-09-02 19:44:59 +0300

    Sidebar ul to div

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,33 +1,48 @@
 <template>
   <div class="sidebar">
-    <ul class="xl-center">
-      <li class="sidebar-list_item">
-        <a href>
-          <img class="sidebar-logo" alt="Vue logo" src="../assets/logo.png" />
-        </a>
-      </li>
-      <li class="sidebar-list_item">
-        <router-link to="/dashboard">
-          <div class="sidebar-list_item_icon">
-            <b-icon type="is-light" icon="home-outline"></b-icon>
+    <div class="sidebar-wrapper wrap xl-1-1 xl-flexbox xl-center xl-between sidebar-list">
+      <div class="col siderbar-top xl-top">
+        <div class="sidebar-list_item logo">
+          <router-link to="/dashboard">
+            <img class="sidebar-logo" alt="Vue logo" src="../assets/logo.png" />
+          </router-link>
+        </div>
+      </div>
+      <div class="col xl-1-1 sidebar-middle">
+        <div class="xl-center">
+          <div class="sidebar-list_item">
+            <router-link to="/dashboard">
+              <div class="sidebar-list_item_icon">
+                <b-icon type="is-light" icon="view-dashboard-outline"></b-icon>
+              </div>
+            </router-link>
           </div>
-        </router-link>
-      </li>
-      <li class="sidebar-list_item">
-        <router-link to="/profile">
-          <div class="sidebar-list_item_icon">
-            <b-icon type="is-light" icon="account-outline"></b-icon>
+          <div class="sidebar-list_item">
+            <router-link to="/messages">
+              <div class="sidebar-list_item_icon">
+                <b-icon type="is-light" icon="message-outline"></b-icon>
+              </div>
+            </router-link>
           </div>
-        </router-link>
-      </li>
-      <li class="sidebar-list_item">
-        <router-link to="/settings">
-          <div class="sidebar-list_item_icon">
-            <b-icon type="is-light" icon="message-settings"></b-icon>
+          <div class="sidebar-list_item">
+            <router-link to="/profile">
+              <div class="sidebar-list_item_icon">
+                <b-icon type="is-light" icon="account-outline"></b-icon>
+              </div>
+            </router-link>
           </div>
-        </router-link>
-      </li>
-    </ul>
+        </div>
+      </div>
+      <div class="col xl-1-1 sidebar-bottom">
+        <div class="sidebar-list_item">
+            <router-link to="/settings">
+              <div class="sidebar-list_item_icon">
+                <b-icon type="is-light" icon="settings"></b-icon>
+              </div>
+            </router-link>
+          </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -37,31 +52,38 @@ export default {
 };
 </script>
 
-<style>
+<style >
 .sidebar {
   height: 100%;
   width: 60px;
   background-color: #000;
   color: rgba(255, 255, 255, 1);
 }
+.sidebar-wrapper {
+  height: 100%;
+}
 
+.sidebar-middle .sidebar-list_item a, .sidebar-bottom .sidebar-list_item a {
+  display: inline-block;
+  width: 50px;
+  padding: 10px;
+  border-radius: .3em;
+}
 .sidebar-logo {
   padding: 10px;
 }
-.sidebar-list_item:nth-child(1) {
-  margin-bottom: 50px;
+.sidebar ul {
+  height: 100%;
 }
 
-.sidebar-list_item:not(:first-child) {
-  overflow: hidden;
-  width: 100%;
-  border-bottom: 1px solid #fff2;
-  padding: 20px 0;
-  font-size: 12px;
+.sidebar-middle .router-link-active, .sidebar-bottom .router-link-active{
+  background-color: var(--primary-dark);
 }
 
+.sidebar-bottom{
+  margin-bottom: 15px;
+}
 .sidebar-list_item:last-child {
   border-bottom: none;
 }
-
 </style>
