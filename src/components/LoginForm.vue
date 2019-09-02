0cd0cbd3baccecefeb2834d89eb0adf50cb8cbe8
Fatih Cüre <curefat@gmail.com>
2019-09-02 19:43:48 +0300

    LoginForm click init: temp. redirects /dashboard

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="login-form_wrap wrap xl-2 xl-center">
     <div class="col xl-1 sm-1-1">
-      <form class="login-form xl-left">
+      <form class="login-form xl-left" @submit="loginSubmit">
         <InputLabel
           type="email"
           placeholder="johndoe@email.com"
@@ -16,16 +16,16 @@
           size="is-medium"
           rounded
           password-reveal
-          />
-        <Button 
-        native-type="submit"
-         size="is-medium"
+        />
+        <Button
+          nativeType="submit"
+          size="is-medium"
           type="is-dark"
-         value="Login" 
-        icon-left="login" 
-        class="is-fullwidth" 
-        rounded
-        style="margin-top: 30px;"
+          value="Login"
+          icon-left="login"
+          class="is-fullwidth"
+          rounded
+          style="margin-top: 30px;"
         />
       </form>
     </div>
@@ -37,16 +37,26 @@ import InputLabel from "./InputLabel.vue";
 import Button from "./Button.vue";
 
 export default {
+  // el:".login-form_wrap",
   name: "LoginForm",
   components: {
     InputLabel,
     Button
+  },
+  methods: {
+     loginSubmit: function (event) {
+       event.preventDefault();
+      // `this` inside methods point to the Vue instance
+      console.log("clicked");
+      this.$router.push('/dashboard')
+      
+    }
   }
 };
 </script>
 
 <style>
-.login-form label{
+.login-form label {
   color: #fff;
 }
 </style>
