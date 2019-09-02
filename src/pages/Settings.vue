e4ac060371d9fc3163929860f6791c54ef1fb4ca
Fatih Cüre <curefat@gmail.com>
2019-09-03 00:15:41 +0300

    Settings page init

---
+++ src/pages/Settings.vue
@@ -0,0 +1,21 @@
+<template>
+  <DefaultLayout>
+   <div class="settings">
+     <p> this is settings</p>
+   </div>
+  </DefaultLayout>
+</template>
+
+<script>
+import DefaultLayout from "../layouts/DefaultLayout";
+export default {
+  name: "Settings",
+  components:{
+    DefaultLayout
+  }
+}
+</script>
+
+<style>
+
+</style>
