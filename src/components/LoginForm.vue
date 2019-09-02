e8465fad5d61706d6cef085d4774f71633262bc9
Fatih Cüre <curefat@gmail.com>
2019-09-02 12:49:34 +0300

    Updated with buefy

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,52 +1,52 @@
 <template>
-  <div class="login-form wrap xl-2 xl-center">
-    <div class="col xl-1">
-      <form>
-        <Input
+  <div class="login-form_wrap wrap xl-2 xl-center">
+    <div class="col xl-1 sm-1-1">
+      <form class="login-form xl-left">
+        <InputLabel
           type="email"
           placeholder="johndoe@email.com"
           labelText="email"
-          inputStyle="text-align: center;"
+          size="is-medium"
+          rounded
         />
-        <Input
+        <InputLabel
           type="password"
-          placeholder="......"
+          placeholder="....."
           labelText="password"
-          inputStyle="text-align: center;"
+          size="is-medium"
+          rounded
+          password-reveal
           />
-        <Button type="submit" value="Login" style="width: calc(100% - 20px); margin-top: 30px;" />
+        <Button 
+        native-type="submit"
+         size="is-medium"
+          type="is-dark"
+         value="Login" 
+        icon-left="login" 
+        class="is-fullwidth" 
+        rounded
+        style="margin-top: 30px;"
+        />
       </form>
     </div>
   </div>
 </template>
 
 <script>
-import Input from "./Input.vue";
+import InputLabel from "./InputLabel.vue";
 import Button from "./Button.vue";
 
 export default {
   name: "LoginForm",
   components: {
-    Input,
+    InputLabel,
     Button
   }
 };
 </script>
 
 <style>
-html,
-body {
-  width: 100%;
-  height: 100%;
-}
-body {
-  background-color: #292929;
-}
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.login-form label{
   color: #fff;
-  height: 100%;
 }
 </style>
