cabeee83699862e6809e45558d5bf02a8a33b2ce
Fatih Cüre <curefat@gmail.com>
2019-09-03 00:08:50 +0300

    DefaultLayout added

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="app-wrapped xl-flexbox">
+    <div class="sidebar-wrapped">
+      <Sidebar />
+    </div>
+    <div class="content-wrapped">
+      <transition name="moveInUp">
+        <slot />
+      </transition>
+    </div>
+  </div>
+</template>
+
+<script>
+// components
+import Sidebar from "../components/Sidebar";
+
+export default {
+  name: "DefaultLayout",
+  components: {
+    Sidebar
+  }
+};
+</script>
+
+<style scoped>
+.app-wrapped {
+  width: 100%;
+  height: 100%;
+  /* display: flex; */
+}
+.sidebar-wrapped {
+  height: 100%;
+  width: 60px;
+}
+.content-wrapped {
+  height: 100%;
+  flex: 1 1 0;
+  font-size: 12px;
+  color: #fff;
+  overflow: auto;
+}
+</style>
