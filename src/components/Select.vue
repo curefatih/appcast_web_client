125b981c930c395d9c9a63b796eb0e8a213794d4
Fatih Cüre <curefat@gmail.com>
2019-09-29 01:00:48 +0300

    Select component

---
+++ src/components/Select.vue
@@ -0,0 +1,36 @@
+<template>
+  <b-field>
+    <b-select class="f_select" v-bind="$attrs">
+      <slot />
+    </b-select>
+  </b-field>
+</template>
+
+<script>
+export default {
+  name: "Select"
+  // props: {
+  //   placeholder: String
+  // }
+};
+</script>
+
+<style>
+.f_select select {
+  background-color: var(--primary-dark);
+  border: 1px solid transparent;
+  color: #555;
+  /* font-size: 1rem; */
+  transition: .2s all;
+}
+
+.f_select select:hover {
+  background-color: var(--primary-dark);
+  border: 1px solid #666;
+}
+.f_select select:focus {
+  background-color: #333;
+  border: 1px solid #999;
+  color: #fff;
+}
+</style>>
