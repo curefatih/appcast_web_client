b5f8dc6072751d2633ca3b7354329cb2bdab1641
Fatih Cüre <curefat@gmail.com>
2019-09-29 18:55:55 +0300

    Settings page added

---
+++ src/pages/subPages/SettingsAccount.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="account-settings">
+    <div class="account-settings_list">
+      <!-- <div class="sec-header">
+        <h4>Account</h4>
+      </div> -->
+      <div class="sec-content">
+        <div class="wrap xl-flexbox xl-middle xl-gutter-8">
+          <div class="col xl-3-12">Username</div>
+          <div class="col xl-9-12">
+            <DefaultInput placeholder="@curefatih" />
+          </div>
+        </div>
+        <div class="wrap xl-flexbox xl-middle xl-gutter-8">
+          <div class="col xl-3-12">Email</div>
+          <div class="col xl-9-12">
+            <DefaultInput placeholder="contact@fatihcure.com" />
+          </div>
+        </div>
+        <div class="wrap xl-flexbox xl-middle xl-gutter-8">
+          <div class="col xl-3-12">Password</div>
+          <div class="col xl-9-12">
+            <div class="wrap xl-gutter-8">
+              <div class="col xl-1-1">
+                <DefaultInput placeholder="current password" type="password" />
+              </div>
+              <div class="col xl-1-1">
+                <DefaultInput placeholder="new password" type="password" />
+              </div>
+            </div>
+            <div class="col xl-1-1">
+              <DefaultInput placeholder="new password" type="password" />
+            </div>
+          </div>
+        </div>
+        <div class="wrap xl-flexbox xl-middle xl-gutter-8">
+          <div class="col xl-3-12">Language</div>
+          <div class="col xl-9-12">
+            <Select placeholder="Language" expanded>
+              <option
+                v-for="option in data"
+                :value="option.id"
+                :key="option.id"
+              >{{ option.language }}</option>
+            </Select>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import DefaultInput from "../../components/DefaultInput";
+import Select from "../../components/Select";
+export default {
+  name: "SettingsAccount",
+  components: {
+    DefaultInput,
+    Select
+  },
+  data() {
+    return {
+      data: [
+        {
+          id: 1,
+          language: "English"
+        },
+        {
+          id: 2,
+          language: "Turkish"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/pages/subPages/SettingsNotFound.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="not-found">
+    <div class="wrap xl-center">
+      <div class="col xl-1-1">
+        <h1>Sorry, that page doesn’t exist!</h1>
+        <h4>Why not try a search to find something else?</h4>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SettingsNotFound"
+};
+</script>
+
+<style>
+</style>
